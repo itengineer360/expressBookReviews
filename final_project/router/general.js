ca0b09a45874b2be2798c47ffb02c230d3cc3081
itengineer360 <itengineer360@github.com>
2025-01-29 11:29:00 -0500

    general updated

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,21 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  return res.status(300).json(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author
+  return res.status(300).json(books[author]);
 });
 
 // Get all books based on title
